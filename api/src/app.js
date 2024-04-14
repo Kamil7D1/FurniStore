c9c15127959c2d2e9960960ba3ab7e7c9ff5828d
kamil <kamilcz2001@gmail.com>
2024-04-14 14:04:11 +0200

    added routes api/auth api/user api/products

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
+import authRoute from "./routes/auth.route.js";
+import userRoute from "./routes/user.route.js";
+import productsRoute from "./routes/products.route.js";
 
 const app = express();
 
+app.use("api/auth", authRoute);
+app.use("api/user", userRoute);
+app.use("api/products", productsRoute);
 app.listen(3000, () => {
     console.log("Server is running!");
 });
