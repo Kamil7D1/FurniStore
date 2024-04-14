7c7df8cd958a3d133363063ad01abe4035b2927c
kamil <kamilcz2001@gmail.com>
2024-04-14 16:01:38 +0200

    added create user feature

--- api/src/auth.controller.js
+++ api/src/auth.controller.js
@@ -1,11 +1,27 @@
 import bcrypt from "bcrypt";
+import prisma from './lib/prisma.js';
 
 export const register = async (req, res) => {
     const {username, email, password} = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 8);
+    try {
+        const hashedPassword = await bcrypt.hash(password, 8);
 
-    console.log(hashedPassword);
+        console.log(hashedPassword);
+
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+            },
+        });
+
+        res.status(201).json({message: "User created."});
+    } catch (err){
+        console.log(err);
+        res.status(500).json({message: "Failed to create user."})
+    }
 };
 
 export const login = (req, res) => {
